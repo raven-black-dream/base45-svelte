aa29a6539e70bf9107377b86b8ae11efd9ab149a
Evan Harley <evan.harley21@gmail.com>
2024-01-15 16:27:01 -0800

    Template List Accordion

--- src/routes/programs/templateslist/+page.svelte
+++ src/routes/programs/templateslist/+page.svelte
@@ -2,6 +2,7 @@
 
 <script lang="ts">
 	export let data
+    import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
 </script>
 
 <!-- TODO: this page will be for listing the program templates, and also link to building a mesocycle -->
@@ -12,39 +13,41 @@
 
 <ul>
 	{#each data.programs as program}
-        <div class="card variant-ghost-primary">
-		<header class="card-header">{program.template_name}</header>
-        <section class="p-4">
-            <div class="table-container">
-                <table class="table table-hover">
-                    <thead>
-                        <tr>
-                            <th>Day</th>
-                            <th>Muscle Groups</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {#each program.template_day as day}
-                            <tr>
-                                <td>{day.template_day_name}</td>
-                                <td>
-                                    <ul>
-                                        {#each day.template_muscle_group as template_muscle_group}
-                                            <li>{template_muscle_group.muscle_group}</li>
-                                        {/each}
-                                    </ul>
-                                </td>
-                            </tr>
-                        {/each}
-                    </tbody>
-                </table>
-            </div>
-        </section>
-        <footer class="card-footer">
-            <a class="btn btn-sm variant-ghost-primary" href="/programs/templateslist/{program.id}">
-                Create a Mesocycle
-            </a>
-        </footer>
-        </div>
+        <Accordion>
+            <AccordionItem class="card variant-glass-primary">
+                <svelte:fragment slot="lead">
+                    <a class="btn btn-sm variant-ghost-secondary" href="/programs/templateslist/{program.id}">
+                        Create a Mesocycle
+                    </a>
+                </svelte:fragment>
+                <svelte:fragment slot="summary" >{program.template_name}</svelte:fragment>
+                <svelte:fragment slot="content" >
+                    <div class="table-container">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Day</th>
+                                    <th>Muscle Groups</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {#each program.template_day as day}
+                                    <tr>
+                                        <td>{day.template_day_name}</td>
+                                        <td>
+                                            <ul>
+                                                {#each day.template_muscle_group as template_muscle_group}
+                                                    <li>{template_muscle_group.muscle_group}</li>
+                                                {/each}
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                {/each}
+                            </tbody>
+                        </table>
+                    </div>
+                </svelte:fragment>
+        </AccordionItem>
+    </Accordion>
 	{/each}
 </ul>
