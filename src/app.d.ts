3a887d16cbbcd6860413de842dbe9d7bf6f39c8d
Evan Harley <evan.harley21@gmail.com>
2024-05-23 16:03:29 -0700

    upgraded to fix issues

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,16 +2,25 @@
 // for information about these interfaces
 // and what to do when importing types
 
-import { SupabaseClient, Session } from '@supabase/supabase-js'
+import { SupabaseClient, Session } from "@supabase/supabase-js";
 
 declare global {
   namespace App {
     interface Locals {
-      supabase: SupabaseClient
-      getSession(): Promise<Session | null>
+      supabase: SupabaseClient;
+      getSession(): Promise<Session | null>;
     }
     interface PageData {
-      session: Session | null
+      session: Session | null;
+    }
+    interface Platform {
+      env: {
+        COUNTER: DurableObjectNamespace;
+      };
+      context: {
+        waitUntil(promise: Promise<any>): void;
+      };
+      caches: CacheStorage & { default: Cache };
     }
     // interface Error {}
     // interface Platform {}
