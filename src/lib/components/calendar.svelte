72d110887d6b4f7bcfd02a59a5972be98bfa395d
Katland <code.katland@gmail.com>
2024-05-28 21:11:26 -0700

    squish the calendar down on smaller screens

--- src/lib/components/calendar.svelte
+++ src/lib/components/calendar.svelte
@@ -52,7 +52,7 @@
 .calendar {
   display: grid;
   width: 100%;
-  grid-template-columns: repeat(7, minmax(120px, 1fr));
+  grid-template-columns: repeat(7, minmax(14%, 1fr));
   grid-template-rows: 50px;
   grid-auto-rows: 120px;
   overflow: auto;
