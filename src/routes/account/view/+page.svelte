fa8d098226fea0105a4d99203fe43207ed995680
Evan Harley <evan.harley21@gmail.com>
2024-07-18 14:57:36 -0700

    Moved workout History to its own page.

--- src/routes/account/view/+page.svelte
+++ src/routes/account/view/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import WorkoutHistoryBlock from '../../../lib/components/WorkoutHistoryBlock.svelte';
+
     import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
     import type { PageData } from './$types';
     import LinePlot from '$lib/components/LinePlot.svelte';
@@ -56,27 +58,6 @@
                         </div>
 
                     </form>
-
-                </svelte:fragment>
-            </AccordionItem>
-            <AccordionItem>
-                <svelte:fragment slot='summary'>
-                    WorkoutHistory
-                </svelte:fragment>
-                <svelte:fragment slot='content'>
-                <div class="snap-x scroll-px-4 snap-mandatory scroll-smooth flex gap-4 overflow-x-auto px-4 py-10">
-                    {#each data.workoutHistory as workout, i (workout.id)}
-                        <div class="snap-start shrink-0 card py-20 w-40 md:w-80 text-center variant-filled-surface">
-                            <header class='card-header'>{new Date(workout.date).toDateString()}</header>
-                            <section class='p-r'>
-                                <a class='text-xs md:text-base' href="/workout/{workout.id}">{workout.day_name}</a>
-                            </section>
-
-                        </div>
-                    {/each}
-	
-                </div>
-                    
                 </svelte:fragment>
             </AccordionItem>
         </Accordion>
