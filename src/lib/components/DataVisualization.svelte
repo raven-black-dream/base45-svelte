182ee5dff0d2a3f45e11a11ba48ee4a998d7effc
Evan Harley <evan.harley21@gmail.com>
2024-09-11 00:25:48 -0700

    trying not to be stupid

---
+++ src/lib/components/DataVisualization.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { onMount } from 'svelte';
+    import Plot from 'svelte-plotly.js';
+
+    export let data;
+
+    onMount(async () => {
+        console.log(data);
+    });
+
+</script>
+
+<Plot {data} layout={{margin: { t: 0 }, paper_bgcolor: '#333333', plot_bgcolor: '#333333', font: {color: '#cccccc'}, xaxis: {color: '#cccccc'}, yaxis: {color: '#cccccc'}}} fillParent='width' debounce={250}/>
