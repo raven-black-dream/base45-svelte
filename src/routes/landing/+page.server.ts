c8951a25c4d76d5435a4306292421f6f8aebaacf
Evan Harley <evan.harley21@gmail.com>
2024-09-13 10:16:25 -0700

    small refactor

--- src/routes/landing/+page.server.ts
+++ src/routes/landing/+page.server.ts
@@ -1,10 +1,16 @@
 // src/routes/landing/+page.server.ts
 
 import { redirect } from "@sveltejs/kit";
-import  prisma from "$lib/server/prisma";
+import prisma from "$lib/server/prisma";
 
+interface MetricData {
+  metric_name: string;
+  value: number;
+  workouts: { date: Date };
+  exercises: { exercise_name: string; muscle_group: string };
+}
 
-export const load = async ({ locals: { supabase, getSession } }) => {
+export const load = async ({ locals: { supabase } }) => {
   const {
     data: { user },
   } = await supabase.auth.getUser();
@@ -15,32 +21,28 @@ export const load = async ({ locals: { supabase, getSession } }) => {
 
   let { firstDay, lastDay } = getCurrentWeek();
 
-  const mesocycle = await prisma.mesocycle.findFirst(
-    {
-      where: {
-        user: user.id,
-        current: true,
-      },
-      select: {
-        id: true,
-        start_date: true,
-        workouts_workouts_mesocycleTomesocycle: {
-          select: {
-            id: true,
-            day_name: true,
-            date: true,
-            complete: true
-          },
-          orderBy: {
-            date: "asc"
-          }
+  const mesocycle = await prisma.mesocycle.findFirst({
+    where: {
+      user: user.id,
+      current: true,
+    },
+    select: {
+      id: true,
+      start_date: true,
+      workouts_workouts_mesocycleTomesocycle: {
+        select: {
+          id: true,
+          day_name: true,
+          date: true,
+          complete: true,
         },
-        meso_day_meso_day_mesocycleTomesocycle: true
-    }
-
-    }
-
-  );
+        orderBy: {
+          date: "asc",
+        },
+      },
+      meso_day_meso_day_mesocycleTomesocycle: true,
+    },
+  });
   if (!mesocycle) {
     console.log("No mesocycle found for the current user.");
     return { user, workouts: [], numberOfDays: 0 };
@@ -57,9 +59,9 @@ export const load = async ({ locals: { supabase, getSession } }) => {
   let numberOfDays = mesoDay?.length || 0;
 
   const nextWorkouts = workouts
-  .filter(workout => !workout.complete)
-  .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
-  .slice(0, numberOfDays);
+    .filter((workout) => !workout.complete)
+    .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+    .slice(0, numberOfDays);
 
   let numComplete = 0;
   workouts.forEach((workout) => {
@@ -76,7 +78,7 @@ export const load = async ({ locals: { supabase, getSession } }) => {
   });
 
   const lastCompletedWorkout = workouts
-    .filter(workout => workout.complete)
+    .filter((workout) => workout.complete)
     .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
     .pop();
 
@@ -86,7 +88,7 @@ export const load = async ({ locals: { supabase, getSession } }) => {
 
   const metricData = await prisma.user_exercise_metrics.findMany({
     where: {
-      mesocycle: mesocycle.id
+      mesocycle: mesocycle.id,
     },
     select: {
       metric_name: true,
@@ -94,17 +96,16 @@ export const load = async ({ locals: { supabase, getSession } }) => {
       exercises: {
         select: {
           exercise_name: true,
-          muscle_group: true
-        }
+          muscle_group: true,
+        },
       },
       workouts: {
         select: {
-          date: true
-        }
-      }
-
-    }
-  })
+          date: true,
+        },
+      },
+    },
+  });
 
   let currentFatigueScore: number;
   let previousFatigueScore: number;
@@ -114,95 +115,95 @@ export const load = async ({ locals: { supabase, getSession } }) => {
   let previousTotalLoad: number;
 
   if (!metricData) {
-
     currentFatigueScore = 0;
     previousFatigueScore = 0;
     currentStimulusScore = 0;
     previousStimulusScore = 0;
     currentTotalLoad = 0;
     previousTotalLoad = 0;
-
   }
-  
-  const lastWeekFirstDay = new Date(firstDay.getTime() - (7 * 24 * 60 * 60 * 1000));
-  const lastWeekCurrentDay = new Date(new Date().getTime() - (7 * 24 * 60 * 60 * 1000));
-
-  const fatigueScores = metricData
-  .filter(metric => metric.metric_name === 'fatigue_score')
-  .sort((a, b) => new Date(a.workouts.date).getTime() - new Date(b.workouts.date).getTime());
-
-  const currentFatigueScores = fatigueScores
-  .filter(
-    metric => metric.workouts?.date?.getTime() >= firstDay.getTime() && metric.workouts?.date?.getTime() <= new Date().getTime()
-  )
-  currentFatigueScore = currentFatigueScores.reduce((sum, metric) => sum + metric.value, 0) / currentFatigueScores.length;
-
-  const previousFatigueScores = fatigueScores
-  .filter(
-    metric => metric.workouts?.date?.getTime() >= lastWeekFirstDay && metric.workouts?.date?.getTime() <= lastWeekCurrentDay
-  )
-  previousFatigueScore = previousFatigueScores.reduce((sum, metric) => sum + metric.value, 0) / previousFatigueScores.length;
-
-  const stimulusScores = metricData.filter(metric => metric.metric_name === 'raw_stimulus_magnitude');
-
-  const currentStimulusScores = stimulusScores
-  .filter(
-    metric => metric.workouts?.date?.getTime() >= firstDay.getTime() && metric.workouts?.date?.getTime() <= new Date().getTime()
-  )
-  currentStimulusScore = currentStimulusScores.reduce((sum, metric) => sum + metric.value, 0) / currentStimulusScores.length;
-
-  const previousStimulusScores = stimulusScores
-  .filter(
-    metric => metric.workouts?.date?.getTime() >= lastWeekFirstDay && metric.workouts?.date?.getTime() <= lastWeekCurrentDay
-  )
-  previousStimulusScore = previousStimulusScores.reduce((sum, metric) => sum + metric.value, 0) / previousStimulusScores.length;
-
-  const totalLoads = metricData.filter(metric => metric.metric_name === 'total_weight');
-
-  const currentTotalLoads = totalLoads
-  .filter(
-    metric => metric.workouts?.date?.getTime() >= firstDay.getTime() && metric.workouts?.date?.getTime() <= new Date().getTime()
-  )
-  currentTotalLoad = currentTotalLoads.reduce((sum, metric) => sum + metric.value, 0);
-
-  const previousTotalLoads = totalLoads
-  .filter(
-    metric => metric.workouts?.date?.getTime() >= lastWeekFirstDay && metric.workouts?.date?.getTime() <= lastWeekCurrentDay
-  )
-  previousTotalLoad = previousTotalLoads.reduce((sum, metric) => sum + metric.value, 0);
-
-
+  const { currentWeekStart, lastWeekStart, lastWeekEnd } =
+    calculateDateRanges(firstDay);
+  const caluclatedMetrics = {
+    fatigue: calculateMetric(
+      metricData,
+      "fatigue_score",
+      currentWeekStart,
+      lastWeekStart,
+      lastWeekEnd,
+    ),
+    stimulus: calculateMetric(
+      metricData,
+      "raw_stimulus_magnitude",
+      currentWeekStart,
+      lastWeekStart,
+      lastWeekEnd,
+    ),
+    totalLoad: calculateTotalLoad(
+      metricData,
+      currentWeekStart,
+      lastWeekStart,
+      lastWeekEnd,
+    ),
+  };
 
   const weeklyMetrics = [
-    [{
-      type: 'indicator',
-      mode: 'number+delta',
-      value: currentFatigueScore,
-      number: {font: {size: 40}},
-      delta: {reference: previousFatigueScore, valueformat: '.2f', increasing: { color: '#730000'}, decreasing: { color: '#29712d'}},
-      title: { text: 'Weekly Fatigue Score' }
-    }],
-    [{
-      type: 'indicator',
-      mode: 'number+delta',
-      value: currentStimulusScore,
-      number: {font: {size: 40}},
-      delta: {reference: previousStimulusScore, valueformat: '.2f', increasing: { color: '#29712d'}, decreasing: { color: '#730000'}},
-      title: { text: 'Weekly Stimulus Score' }
-    }],
-    [{
-      type: 'indicator',
-      mode: 'number+delta',
-      value: currentTotalLoad,
-      number: {font: {size: 40}},
-      delta: {reference: previousTotalLoad, valueformat: '.0f', increasing: { color: '#29712d'}, decreasing: { color: '#730000'}},
-      title: { text: 'Total Load Score' }
-    }],
-
+      [
+        {
+          type: "indicator",
+          mode: "number+delta",
+          value: caluclatedMetrics.totalLoad.current,
+          number: { font: { size: 40 } },
+          delta: {
+            reference: caluclatedMetrics.totalLoad.previous,
+            valueformat: ".0f",
+            increasing: { color: "#29712d" },
+            decreasing: { color: "#730000" },
+          },
+          title: { text: "Total Load Score" },
+        },
+      ],
+      [
+      {
+        type: "indicator",
+        mode: "number+delta",
+        value: caluclatedMetrics.fatigue.current,
+        number: { font: { size: 40 } },
+        delta: {
+          reference: caluclatedMetrics.fatigue.previous,
+          valueformat: ".2f",
+          increasing: { color: "#730000" },
+          decreasing: { color: "#29712d" },
+        },
+        title: { text: "Weekly Fatigue Score" },
+      },
+    ],
+    [
+      {
+        type: "indicator",
+        mode: "number+delta",
+        value: caluclatedMetrics.stimulus.current,
+        number: { font: { size: 40 } },
+        delta: {
+          reference: caluclatedMetrics.stimulus.previous,
+          valueformat: ".2f",
+          increasing: { color: "#29712d" },
+          decreasing: { color: "#730000" },
+        },
+        title: { text: "Weekly Stimulus Score" },
+      },
+    ],
   ];
 
-
-  return { user, workouts, nextWorkouts, numberOfDays, numComplete, currentWeek, weeklyMetrics };
+  return {
+    user,
+    workouts,
+    nextWorkouts,
+    numberOfDays,
+    numComplete,
+    currentWeek,
+    weeklyMetrics,
+  };
 };
 
 function getCurrentWeek() {
@@ -215,7 +216,100 @@ function getCurrentWeek() {
 }
 
 function calculateWeekNumber(start_date: Date, reference_date: Date) {
-  const timeDifference = Math.abs(reference_date.getTime() - start_date.getTime());
+  const timeDifference = Math.abs(
+    reference_date.getTime() - start_date.getTime(),
+  );
   const weeks = Math.ceil(timeDifference / (1000 * 60 * 60 * 24 * 7));
   return weeks;
+}
+
+function calculateDateRanges(firstDay: Date) {
+  const currentWeekStart = firstDay;
+  const currentDate = new Date();
+  const lastWeekStart = new Date(firstDay.getTime() - 7 * 24 * 60 * 60 * 1000);
+  const lastWeekEnd = new Date(currentDate.getTime() - 7 * 24 * 60 * 60 * 1000);
+
+  return { currentWeekStart, lastWeekStart, lastWeekEnd };
+}
+
+function calculateMetric(
+  metricData: MetricData[],
+  metricName: string,
+  currentWeekStart: Date,
+  lastWeekStart: Date,
+  lastWeekEnd: Date,
+) {
+  const relevantMetrics = metricData
+    .filter((metric) => metric.metric_name === metricName)
+    .sort((a, b) => a.workouts.date.getTime() - b.workouts.date.getTime());
+
+  const current = calculateAverageForDateRange(
+    relevantMetrics,
+    currentWeekStart,
+    new Date(),
+  );
+  const previous = calculateAverageForDateRange(
+    relevantMetrics,
+    lastWeekStart,
+    lastWeekEnd,
+  );
+
+  return { current, previous };
+}
+
+function calculateTotalLoad(
+  metricData: MetricData[],
+  currentWeekStart: Date,
+  lastWeekStart: Date,
+  lastWeekEnd: Date,
+) {
+  const totalLoads = metricData.filter(
+    (metric) => metric.metric_name === "total_weight",
+  );
+
+  const current = calculateSumForDateRange(
+    totalLoads,
+    currentWeekStart,
+    new Date(),
+  );
+  const previous = calculateSumForDateRange(
+    totalLoads,
+    lastWeekStart,
+    lastWeekEnd,
+  );
+
+  return { current, previous };
+}
+
+function calculateAverageForDateRange(
+  metrics: MetricData[],
+  startDate: Date,
+  endDate: Date,
+): number {
+  const filteredMetrics = filterMetricsByDateRange(metrics, startDate, endDate);
+  return filteredMetrics.length > 0
+    ? filteredMetrics.reduce((sum, metric) => sum + metric.value, 0) /
+        filteredMetrics.length
+    : 0;
+}
+
+function calculateSumForDateRange(
+  metrics: MetricData[],
+  startDate: Date,
+  endDate: Date,
+): number {
+  const filteredMetrics = filterMetricsByDateRange(metrics, startDate, endDate);
+  return filteredMetrics.reduce((sum, metric) => sum + metric.value, 0);
+}
+
+function filterMetricsByDateRange(
+  metrics: MetricData[],
+  startDate: Date,
+  endDate: Date,
+): MetricData[] {
+  return metrics.filter(
+    (metric) =>
+      metric.workouts?.date?.getTime() >= startDate.getTime() &&
+      metric.workouts?.date?.getTime() <= endDate.getTime(),
+  );
 }
