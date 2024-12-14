66f0e80b902bfdb12f5c7a6660da241c47edadbf
Evan Harley <evan.harley21@gmail.com>
2024-12-13 16:59:46 -0800

    added cosmetic features to workout row and workout metrics

--- src/routes/workout/summary/[slug]/+page.svelte
+++ src/routes/workout/summary/[slug]/+page.svelte
@@ -8,6 +8,7 @@ import Icon from '@iconify/svelte';
     }
 
     let { data }: Props = $props();
+    let value = $state([])
 </script>
 
 
@@ -19,26 +20,80 @@ import Icon from '@iconify/svelte';
     <header class='card-header text-xl font-extrabold'>
         {data.workout?.day_name} - {new Date(data.workout?.date).toDateString()}
     </header>
-    <section class='p-4 space-y-4'>        
+    <Accordion {value} collapsible>        
         <div class='space-y-4'>
             {#each Object.entries(data.setData) as [muscleGroup, exercises]}
             <div class='card p-4 preset-tonal-primary preset-outlined-primary-200-800'>
                 <header class='card-header p-4 text-xl font-extrabold'>{muscleGroup}</header>
                 <section>
                     {#each Object.entries(exercises) as [index, exercise]}
-                    <div class='p-2'>
-                        <p class='card-header text-lg font-bold text-center'>{index}</p>
-                        <div class='p-4 flex items-center justify-center'>
-                            <ul>
-                                <li>
-                                    <p>Weight: {exercise.weight}</p>
-                                </li>
-                                <li>
-                                    <p>Reps: {exercise.reps}</p>
-                                </li>
-                            </ul>
+                        <Accordion.Item value={index} class='p-2'>
+                        {#snippet lead()}
+                            <Icon icon='fa6-solid:dumbbell'/>
+                        {/snippet}
+                        {#snippet control()}
+                            <p class='card-header text-lg font-bold'>{index}</p>
+                            <p>Weight: {exercise.weight}</p>
+                            <p>Reps: {exercise.reps}</p>
+                        {/snippet}
+                        {#snippet panel()}
+                        <div class='card p-4 preset-filled-surface-100-900'>
+                            <div>
+                                <header class='card-header text-lg font-bold text-center'>Rep Metrics</header>
+                                <div class='grid grid-cols-3 gap-1'>
+                                    <div></div>
+                                    <div class='text-center'><header>Actual</header></div>
+                                    <div class='text-center'><header>Expected</header></div>
+                                    
+                                    <div>
+                                        <p>Total:</p>
+                                        <p>Average:</p>
+                                        <p>Variation:</p>
+                                    </div>
+                                    <div class='text-center'>
+                                        <p>{exercise.metrics['total_reps']}</p>
+                                        <p>{exercise.metrics['average_reps']}</p>
+                                        <p>{Math.round(exercise.metrics['rep_std_dev'] * 100)/100}</p>
+                                    </div>
+                                    <div class='text-center'>
+                                        <p>{exercise.metrics['expected_reps']}</p>
+                                        <p>{exercise.metrics['expected_average_reps']}</p>
+                                        <p>-</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <header class='card-header text-lg font-bold text-center'>Weight Metrics</header>
+                                <div class='grid grid-cols-3 gap-1'>
+                                    <div></div>
+                                    <div class='text-center'><header>Actual</header></div>
+                                    <div class='text-center'><header>Expected</header></div>
+
+                                    <div>
+                                        <p>Total:</p>
+                                        <p>Average:</p>
+                                        <p>Variation:</p>
+                                    </div>
+                                    <div class='text-center'>
+                                        <p>{exercise.metrics['total_weight']}</p>
+                                        <p>{exercise.metrics['average_weight']}</p>
+                                        <p>{Math.round(exercise.metrics['weight_std_dev'] * 100)/100}</p>
+                                    </div>
+                                    <div class='text-center'>
+                                        <p>{exercise.metrics['expected_weight']}</p>
+                                        <p>{exercise.metrics['expected_average_weight']}</p>
+                                        <p>-</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <header class='card-header text-lg font-bold text-center'>Other Metrics</header>
+                                <p>Performance Score: {exercise.metrics['performance_score']}</p>
+                            </div>
                         </div>
-                    </div>
+                        {/snippet}
+                        
+                        </Accordion.Item>
                     {/each}
                 </section>
             </div>
@@ -81,7 +136,7 @@ import Icon from '@iconify/svelte';
             
         </div>
 
-    </section>
+    </Accordion>
     <footer class='card-footer'></footer>
 
 </div>
